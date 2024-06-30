2e67f8061b3c49a551f5e1bad7d1a5e65298cf2d
arthvelc <asolares.devml@gmail.com>
2024-06-29 20:11:23 -0600

    creación de las capas del la clase message, messagedao y message services.

--- src/main/java/com/curso/mensajes_app/App.java
+++ src/main/java/com/curso/mensajes_app/App.java
@@ -1,10 +1,55 @@
 package com.curso.mensajes_app;
 import java.sql.Connection;
+import java.util.Scanner;
+import com.curso.mensajes_app.MessagesServices;
 
 
 public class App {
     public static void main( String[] args ){
 
+
+        //Lógica de la aplicación de mensajes con la base de datos en MySQL.
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+
+        do {
+            if(option < 0 || option > 5){
+                System.out.println("Selecciona una opcion valida");
+                option = 0;
+            }else{
+                System.out.println("-----------------");
+                System.out.println("Aplicacion de mensajes");
+                System.out.println("1. Crear mensaje");
+                System.out.println("2. Listar mensajes");
+                System.out.println("3. Editar mensaje");
+                System.out.println("4. Eliminar mensaje");
+                System.out.println("5. Salir");
+
+                //Leer la opcion del usuario
+                option = sc.nextInt();
+
+                switch(option){
+                    case 1:
+                        MessagesServices.createMessage();
+                        break;
+                    case 2:
+                        MessagesServices.listMessages();
+                        break;
+                    case 3:
+                        MessagesServices.updateMessage();
+                        break;
+                    case 4:
+                        MessagesServices.deleteMessage();
+                        break;
+                    default:
+                        break;
+                }
+
+            } 
+        } while (option != 5);
+
+
+        //Prueba de conexion a la base de datos desde main
         Conexion conexion = new Conexion();
         
         try(Connection cnx = conexion.getConnectionApp()){
---
+++ src/main/java/com/curso/mensajes_app/MessagesServices.java
@@ -0,0 +1,23 @@
+package com.curso.mensajes_app;
+
+
+public class MessagesServices {
+
+    public static void createMessage(){
+
+    }
+
+    public static void listMessages(){
+
+    }
+
+    public static void deleteMessage(){
+
+    }
+
+    public static void updateMessage(){
+
+    }
+
+
+}
